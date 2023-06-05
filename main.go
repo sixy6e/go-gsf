21e9e2bb4c45ce879a6582189439a461a1043487
Josh Sixsmith <josh.sixsmith@gmail.com>
2023-06-06 08:53:55 +1000

    Initial outlay of worker pool processing

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
     "log"
     "encoding/json"
     "os"
-    "fmt"
+    //"fmt"
+    "runtime"
+    "os/signal"
+    "context"
 
     "github.com/urfave/cli/v2"
+    "github.com/alitto/pond"
 
     "gsf/decode"
     "gsf/encode"
+    "gsf/search"
 )
 
 // func create_index(gsf_uri string, config_uri string, out_uri string) error {
@@ -23,7 +28,7 @@ func create_index(gsf_uri string, config_uri string) error {
     }
 
     // TODO; if we write the file to a different structure, we need a different extension
-    out_uri := uri + "-index.json"
+    out_uri := gsf_uri + "-index.json"
     _, err = encode.WriteJson(out_uri, config_uri, jsn)
     if err != nil {
         // panic(err)
@@ -33,8 +38,8 @@ func create_index(gsf_uri string, config_uri string) error {
     return nil
 }
 
-func create_index_list(uri, string, config_uri string) error {
-    items := FindGsf(uri, config_uri)
+func create_index_list(uri string, config_uri string) error {
+    items := search.FindGsf(uri, config_uri)
     // out_uris := make([]string, len(items))
 
     // TODO; if we write the file to a different structure, we need a different extension
@@ -42,7 +47,26 @@ func create_index_list(uri, string, config_uri string) error {
     //     out_uris[i] = name + "-index.json"
     // }
 
+    // Create a context that will be cancelled when the user presses Ctrl+C (process receives termination signal).
+    ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+    defer stop()
 
+    // fixed pool
+    n := runtime.NumCPU() * 2
+    pool := pond.New(n, 0,  pond.MinWorkers(n), pond.Context(ctx))
+    defer pool.StopAndWait()
+
+    for _, name := range(items) {
+        item_uri := name
+        pool.Submit(func() {
+            _ = create_index(item_uri, config_uri)
+            // if err != nil {
+            //     return err
+            // }
+        })
+    }
+
+    return nil // TODO; fix this design
 }
 
 func main() {
@@ -67,6 +91,28 @@ func main() {
                 Action: func(cCtx *cli.Context) error {
                     // err := create_index(cCtx.String("gsf-uri"), cCtx.String("config-uri"), cCtx.String("out-uri"))
                     err := create_index(cCtx.String("gsf-uri"), cCtx.String("config-uri"))
+                    return err
+                },
+            },
+            &cli.Command{
+                Name: "index-trawl",
+                Flags: []cli.Flag{
+                    &cli.StringFlag{
+                        Name: "uri",
+                        Usage: "URI or pathname to a directory containing gsf files.",
+                    },
+                    &cli.StringFlag{
+                        Name: "config-uri",
+                        Usage: "URI or pathname to a TileDB config file.",
+                    },
+                    // &cli.StringFlag{
+                    //     Name: "out-uri",
+                    //     Usage: "URI or pathname to write the output file to.",
+                    // },
+                },
+                Action: func(cCtx *cli.Context) error {
+                    // err := create_index(cCtx.String("gsf-uri"), cCtx.String("config-uri"), cCtx.String("out-uri"))
+                    err := create_index_list(cCtx.String("uri"), cCtx.String("config-uri"))
                     return err
                 },
             },
