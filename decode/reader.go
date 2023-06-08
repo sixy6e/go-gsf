954aec844212333479b83c34c8932f0e9cb513a1
Josh Sixsmith <josh.sixsmith@gmail.com>
2023-06-08 13:46:41 +1000

    Initial commit for reader.go

---
+++ decode/reader.go
@@ -0,0 +1,36 @@
+package decode
+
+import (
+    "bytes"
+    "encoding/binary"
+
+    tiledb "github.com/TileDB-Inc/TileDB-Go"
+)
+
+
+// Stream caters for a generic reader type so that we can handle both
+// a stream of data from a file on disk or object store, as well as
+// an in-memory byte stream.
+// This GSF module deals with either a *tiledb.VFSfh or *bytes.Reader,
+// and all we care about are two methods, Read and Seek,
+// which both implement.
+type Stream interface {
+    Read(p []byte) (int, error)
+    Seek(offset int64, whence int) (int64, error)
+}
+
+// function to handle whether we build an in-memory byte stream or leave
+// is as stream handled by *tiledb.VFSfh
+func GenericStream(stream *tiledb.VFSfh, size uint64, inmem bool) Stream {
+    if inmem {
+        buffer := make([]byte, size)
+        err := binary.Read(stream, binary.BigEndian, &buffer)
+        if err != nil {
+            panic(err)
+        }
+        reader := bytes.NewReader(buffer)
+        return reader
+    } else {
+        return stream
+    }
+}
