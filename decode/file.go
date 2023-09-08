27f035918ab021003eb90ccec1a76e456b359fed
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-09-08 16:15:07 +1000

    Larger cleanup; added new type to handle File IO, and converted some funcs to methods.

--- decode/file.go
+++ decode/file.go
@@ -1,10 +1,8 @@
 package decode
 
 import (
-    // "os"
     "encoding/binary"
     "bytes"
-    // "time"
     "fmt"
 
     tiledb "github.com/TileDB-Inc/TileDB-Go"
@@ -41,20 +39,105 @@ type PingGroup struct {
     Stop uint64
 }
 
+type GsfFile struct {
+    Uri string
+    filesize uint64
+    config *tiledb.Config
+    ctx *tiledb.Context
+    vfs *tiledb.VFS
+    handler *tiledb.VFSfh
+    Stream
+}
+
+func OpenGSF(gsf_uri string, config_uri string, in_memory bool) GsfFile {
+    var (
+        gsf GsfFile
+        config *tiledb.Config
+        err error
+    )
+
+    gsf.Uri = gsf_uri
+    
+    // get a generic config if no path provided
+    if config_uri == "" {
+        config, err = tiledb.NewConfig()
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        config, err = tiledb.LoadConfig(config_uri)
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    // defer config.Free()
+    gsf.config = config
+
+    ctx, err := tiledb.NewContext(config)
+    if err != nil {
+        panic(err)
+    }
+    // defer ctx.Free()
+    gsf.ctx = ctx
+
+    vfs, err := tiledb.NewVFS(ctx, config)
+    if err != nil {
+        panic(err)
+    }
+    // defer vfs.Free()
+    gsf.vfs = vfs
+
+    handler, err := vfs.Open(gsf_uri, tiledb.TILEDB_VFS_READ)
+    if err != nil {
+        panic(err)
+    }
+    // defer handler.Close()
+    gsf.handler = handler
+
+    filesize, _ := vfs.FileSize(gsf_uri)
+    gsf.filesize = filesize
+
+    // generic stream
+    stream, err := GenericStream(handler, filesize, in_memory)
+
+    gsf.Stream = stream
+
+    return gsf
+}
+
+// Releases the open tiledb file handler connections.
+func (g *GsfFile) Close() {
+    g.handler.Close()
+    g.vfs.Free()
+    g.ctx.Free()
+    g.config.Free()
+}
+
+type GsfDetails struct {
+    GSF_URI string
+    GSF_Version string
+    Size uint64
+}
+
+type SensorInfo struct {
+    Sensor_ID int32
+    Sensor_Name string
+}
+    
 // FileInfo is the overarching structure containing basic info about the GSF file.
 // Items include file location, file size, counts of each record (main and subrecords),
 // as well as basic info about the pings such as number of beams and schema for each
 // ping contained within the file.
 type FileInfo struct {
-    GSF_URI string
-    Size uint64
-    Sensor_ID int32
-    Sensor_Name string
+    GSF_Details GsfDetails
+    Sensor_Info SensorInfo
     CRS Crs
     SubRecord_Schema []string
     Quality_Info QualityInfo
     Record_Counts map[string]uint64
     SubRecord_Counts map[string]uint64
+    Swath_Summary SwathBathySummary
     Ping_Groups []PingGroup
     Record_Index map[string][]RecordHdr
     Ping_Info []PingInfo
@@ -92,8 +175,7 @@ func (fi *FileInfo) PGroups() {
 
 // Index, as the name implies, builds a file index of all Record types.
 // Each Record contains the record ID, record size, byte index and checksum flag.
-func Index(gsf_uri string, config_uri string, in_memory bool) FileInfo {
-
+func (g *GsfFile) Index() FileInfo {
     var (
         rec_idx map[string][]RecordHdr
         rec_counts map[string]uint64
@@ -101,118 +183,93 @@ func Index(gsf_uri string, config_uri string, in_memory bool) FileInfo {
         sub_rec_counts_str map[string]uint64
         sensor_id int32 
         sensor_name string
-        rec Record
+        rec RecordHdr
         pinfo PingInfo
         pings []PingInfo
         finfo FileInfo
-        config *tiledb.Config
-        err error
+        // err error
         crs Crs
+        buffer []byte
+        reader *bytes.Reader
+        version Header
+        swath_sum SwathBathySummary
     )
 
-    // get a generic config if no path provided
-    if config_uri == "" {
-        config, err = tiledb.NewConfig()
-        if err != nil {
-            panic(err)
-        }
-    } else {
-        config, err = tiledb.LoadConfig(config_uri)
-        if err != nil {
-            panic(err)
-        }
-    }
-
-    defer config.Free()
-
-    ctx, err := tiledb.NewContext(config)
-    if err != nil {
-        panic(err)
-    }
-    defer ctx.Free()
-
-    vfs, err := tiledb.NewVFS(ctx, config)
-    if err != nil {
-        panic(err)
-    }
-    defer vfs.Free()
-
-    handler, err := vfs.Open(gsf_uri, tiledb.TILEDB_VFS_READ)
-    if err != nil {
-        panic(err)
-    }
-    defer handler.Close()
-    // defer stream.Free()
-
-    rec_idx = make(map[string][]Record)
+    rec_idx = make(map[string][]RecordHdr)
     rec_counts = make(map[string]uint64)
     sub_rec_counts = make(map[SubRecordID]uint64)
     sub_rec_counts_str = make(map[string]uint64)
 
     one := uint64(1)
 
-    // filesize is used as an EOF indicator when streaming the raw bytes
-    filesize, _ := vfs.FileSize(gsf_uri)
-
-    // create a generic stream
-    stream, err := GenericStream(handler, filesize, in_memory)
-
     // get the original starting point so we can jump back when done
-    original_pos, _ := Tell(stream)
+    original_pos, _ := Tell(g.Stream)
 
     // start at front of the stream
-    pos, _ := stream.Seek(0, 0)
+    pos, _ := g.Stream.Seek(0, 0)
 
     // reading the bytestream and build record index information
-    for uint64(pos) < filesize {
+    for uint64(pos) < g.filesize {
         // TODO; test that pos moves after we read a header
-        rec = RecordHdr(stream)
+        rec = DecodeRecordHdr(g.Stream)
 
         // increment record count
         rec_counts[RecordNames[rec.Id]] += one
 
         rec_idx[RecordNames[rec.Id]] = append(rec_idx[RecordNames[rec.Id]], rec)
 
-        // TODO; convert to case switch
-        if rec.Id == SWATH_BATHYMETRY_PING {
-            // need to do some sub record decoding
-            buffer := make([]byte, rec.Datasize)
-            _ = binary.Read(stream, binary.BigEndian, &buffer)
-            reader := bytes.NewReader(buffer)
+        switch rec.Id {
+            case SWATH_BATHYMETRY_PING:
+                // need to do some sub record decoding
+                buffer = make([]byte, rec.Datasize)
+                _ = binary.Read(g.Stream, binary.BigEndian, &buffer)
+                reader = bytes.NewReader(buffer)
 
-            pinfo = ping_info(reader, rec)
-            pings = append(pings, pinfo)
+                pinfo = ping_info(reader, rec)
+                pings = append(pings, pinfo)
 
-            // increment sub-record count
-            for _, sid := range(pinfo.Sub_Records) {
-                sub_rec_counts[sid] += one
-            }
+                // increment sub-record count
+                for _, sid := range(pinfo.Sub_Records) {
+                    sub_rec_counts[sid] += one
+                }
 
-            pos, _ = Tell(stream)
-        } else if rec.Id == PROCESSING_PARAMETERS {
-            // should only be one of these records in the gsf file
-            buffer := make([]byte, rec.Datasize)
-            _ = binary.Read(stream, binary.BigEndian, &buffer)
-            params := ProcessingParametersRec(buffer, rec)
+                pos, _ = Tell(g.Stream)
+            case PROCESSING_PARAMETERS:
+                // should only be one of these records in the gsf file
+                buffer = make([]byte, rec.Datasize)
+                _ = binary.Read(g.Stream, binary.BigEndian, &buffer)
 
-            // TODO; change params rec to be a defined struct to avoid this type assertion
-            hd, ok := params["geoid"].(string)
-            if ok {
-                crs.Horizontal_Datum = fmt.Sprint(hd)
-            }
-            vd, ok := params["tidal_datum"].(string)
-            if ok {
-                crs.Vertical_Datum = fmt.Sprint(vd)
-            }
-        } else {
-            // seek over the record and loop to the next
-            pos, _ = stream.Seek(int64(rec.Datasize), 1)
+                params := DecodeProcessingParameters(buffer)
+
+                // TODO; change params rec to be a defined struct to avoid this type assertion
+                hd, ok := params["geoid"].(string)
+                if ok {
+                    crs.Horizontal_Datum = fmt.Sprint(hd)
+                }
+                vd, ok := params["tidal_datum"].(string)
+                if ok {
+                    crs.Vertical_Datum = fmt.Sprint(vd)
+                }
+            case SWATH_BATHY_SUMMARY:
+                buffer = make([]byte, rec.Datasize)
+                _ = binary.Read(g.Stream, binary.BigEndian, &buffer)
+                reader = bytes.NewReader(buffer)
+
+                swath_sum = DecodeSwathBathySummary(reader)
+            case HEADER:
+                buffer = make([]byte, rec.Datasize)
+                _ = binary.Read(g.Stream, binary.BigEndian, &buffer)
+
+                version = DecodeHeader(buffer)
+            default:
+                // seek over the record and loop to the next
+                pos, _ = g.Stream.Seek(int64(rec.Datasize), 1)
         }
 
     }
 
     // reset file posistion
-    _, _ = stream.Seek(original_pos, 0)
+    _, _ = g.Stream.Seek(original_pos, 0)
 
     // consistent schema; we've had cases where the schema is inconsistent between pings
     sr_schema := make([]string, 0)
@@ -226,16 +283,15 @@ func Index(gsf_uri string, config_uri string, in_memory bool) FileInfo {
         }
     }
 
-    finfo.GSF_URI = gsf_uri
-    finfo.Size = filesize
-    finfo.Sensor_ID = sensor_id
-    finfo.Sensor_Name = sensor_name
+    finfo.GSF_Details = GsfDetails{GSF_URI: g.Uri, GSF_Version: version.Version, Size: g.filesize}
+    finfo.Sensor_Info = SensorInfo{Sensor_ID: sensor_id, Sensor_Name: sensor_name}
     finfo.CRS = crs
     finfo.SubRecord_Schema = sr_schema
     finfo.Record_Counts = rec_counts
     finfo.SubRecord_Counts = sub_rec_counts_str
     finfo.Record_Index = rec_idx
     finfo.Ping_Info = pings
+    finfo.Swath_Summary = swath_sum
 
     finfo.PGroups()
     finfo.QInfo()
