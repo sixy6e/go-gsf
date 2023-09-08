0ce4f192cdc85a97a18108d00d236f34c06a48a9
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-09-08 16:11:15 +1000

    Reworked functinal call, and internal var names

--- decode/summary.go
+++ decode/summary.go
@@ -26,8 +26,8 @@ type SwathBathySummary struct {
 // the SWATH_BATHY_SUMMARY record.
 // It contains the geometrical and temporal extent of the swath data contained
 // within the GSF file.
-func DecodeSwathBathySummary(buffer []byte) SwathBathySummary {
-    var buffer2 struct {
+func DecodeSwathBathySummary(reader *bytes.Reader) SwathBathySummary {
+    var buffer struct {
         First_ping_sec int32
         First_ping_nano_sec int32
         Last_ping_sec int32
@@ -40,19 +40,18 @@ func DecodeSwathBathySummary(buffer []byte) SwathBathySummary {
         Max_depth int32
     }
 
-    reader := bytes.DecodeReader(buffer)
-    _ = binary.Read(reader, binary.BigEndian, &buffer2)
+    _ = binary.Read(reader, binary.BigEndian, &buffer)
 
     // should look at storing the scale factors as consts or a struct
     summary := SwathBathySummary{
-        Start_datetime: time.Unix(int64(buffer2.First_ping_sec), int64(buffer2.First_ping_nano_sec)).UTC(),
-        End_datetime: time.Unix(int64(buffer2.Last_ping_sec), int64(buffer2.Last_ping_nano_sec)).UTC(),
-        Min_longitude: float64(float32(buffer2.Min_lon) / SCALE1),
-        Max_longitude: float64(float32(buffer2.Max_lon) / SCALE1),
-        Min_latitude: float64(float32(buffer2.Min_lat) / SCALE1),
-        Max_latitude: float64(float32(buffer2.Max_lat) / SCALE1),
-        Min_depth: float32(buffer2.Min_depth) / SCALE2,
-        Max_depth: float32(buffer2.Max_depth) / SCALE2,
+        Start_datetime: time.Unix(int64(buffer.First_ping_sec), int64(buffer.First_ping_nano_sec)).UTC(),
+        End_datetime: time.Unix(int64(buffer.Last_ping_sec), int64(buffer.Last_ping_nano_sec)).UTC(),
+        Min_longitude: float64(float32(buffer.Min_lon) / SCALE1),
+        Max_longitude: float64(float32(buffer.Max_lon) / SCALE1),
+        Min_latitude: float64(float32(buffer.Min_lat) / SCALE1),
+        Max_latitude: float64(float32(buffer.Max_lat) / SCALE1),
+        Min_depth: float32(buffer.Min_depth) / SCALE2,
+        Max_depth: float32(buffer.Max_depth) / SCALE2,
     }
 
     return summary
