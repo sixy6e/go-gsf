863e9bbb0d696208e0b0d89585ce8493de3b3e0a
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-10-19 09:58:07 +1100

    Initial commit of tiledb.go which will contain a bunch of helper funcs and defined errors

---
+++ tiledb.go
@@ -0,0 +1,119 @@
+package gsf
+
+import (
+    "errors"
+
+    tiledb "github.com/TileDB-Inc/TileDB-Go"
+)
+
+var ErrAddFilters = errors.New("Error Adding Filter To FilterList")
+
+// AddFilters sequentially appends compression filters to the filter pipeline list
+func AddFilters(filter_list *tiledb.FilterList, filter ...*tiledb.Filter) error {
+    for _, filt := range filter {
+        err := filter_list.AddFilter(filt)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
+// ZstdFilter initialises the Zstandard compression filter and sets the compression
+// level.
+func ZstdFilter(ctx *tiledb.Context, level int32) (*tiledb.Filter, error) {
+    filt, err := tiledb.NewFilter(ctx, tiledb.TILEDB_FILTER_ZSTD)
+    if err != nil {
+        return nil, err
+    }
+
+    err = filt.SetOption(tiledb.TILEDB_COMPRESSION_LEVEL, level)
+    if err != nil {
+        filt.Free()
+        return nil, err
+    }
+
+    return filt, nil
+}
+
+// GzipFilter initialises the deflate compression filter and sets the compression
+// level.
+func GzipFilter(ctx *tiledb.Context, level int32) (*tiledb.Filter, error) {
+    filt, err := tiledb.NewFilter(ctx, tiledb.TILEDB_FILTER_GZIP)
+    if err != nil {
+        return nil, err
+    }
+
+    err = filt.SetOption(tiledb.TILEDB_COMPRESSION_LEVEL, level)
+    if err != nil {
+        filt.Free()
+        return nil, err
+    }
+
+    return filt, nil
+}
+
+// Lz4Filter initialises the LZ4 compression filter and sets the compression
+// level.
+func Lz4Filter(ctx *tiledb.Context, level int32) (*tiledb.Filter, error) {
+    filt, err := tiledb.NewFilter(ctx, tiledb.TILEDB_FILTER_LZ4)
+    if err != nil {
+        return nil, err
+    }
+
+    err = filt.SetOption(tiledb.TILEDB_COMPRESSION_LEVEL, level)
+    if err != nil {
+        filt.Free()
+        return nil, err
+    }
+
+    return filt, nil
+}
+
+// RleFilter initialises the Run Length Encoding compression filter and sets the
+// compression level.
+func RleFilter(ctx *tiledb.Context, level int32) (*tiledb.Filter, error) {
+    filt, err := tiledb.NewFilter(ctx, tiledb.TILEDB_FILTER_RLE)
+    if err != nil {
+        return nil, err
+    }
+
+    err = filt.SetOption(tiledb.TILEDB_COMPRESSION_LEVEL, level)
+    if err != nil {
+        filt.Free()
+        return nil, err
+    }
+
+    return filt, nil
+}
+
+// Bzip2Filter initialises the Burrows-Wheeler compression filter and sets the
+// compression level.
+func Bzip2Filter(ctx *tiledb.Context, level int32) (*tiledb.Filter, error) {
+    filt, err := tiledb.NewFilter(ctx, tiledb.TILEDB_FILTER_BZIP2)
+    if err != nil {
+        return nil, err
+    }
+
+    err = filt.SetOption(tiledb.TILEDB_COMPRESSION_LEVEL, level)
+    if err != nil {
+        filt.Free()
+        return nil, err
+    }
+
+    return filt, nil
+}
+
+// AttachFilters acts as a helper for when setting the same pipeline filter list to
+// a bunch of attributes.
+func AttachFilters(filter_list *tiledb.FilterList, attrs ...*tiledb.Attribute) error {
+    for _, attr := range attrs {
+         err := attr.SetFilterList(filter_list)
+         if err != nil {
+             return err
+         }
+    }
+
+    return nil
+}
