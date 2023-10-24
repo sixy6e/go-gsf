ff6479383873f26f3ddc41ac08b951b10f373445
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2023-10-24 22:23:31 +1100

    Additional docstrings; fixed incorrect var name; defined deg2rad as go doesn't have an inbuilt radians func

--- geo.go
+++ geo.go
@@ -21,8 +21,10 @@ type GeoCoefficients struct {
 }
 
 // NewCoefWgs84 initialises a GeoCoefficients with coefficients set for WGS84.
+// No thoughts, as of yet, to generate coefficients for other datums.
+// Or is an alternative method more suited.
 func NewCoefWgs84 () *GeoCoefficients {
-    g := new(GeoCoeficients)
+    g := new(GeoCoefficients)
     g.A = 111132.92
     g.B = 559.82
     g.C = 1.175
@@ -41,31 +43,35 @@ type LonLat struct {
 
 // BeamsLonLat calculates arrays of longitude and latitude of len(along_track).
 // Most likely the func will change; potentially a method for ping data, header or GeoCoefficients.
+// For formulae details: https://gis.stackexchange.com/questions/75528/understanding-terms-in-length-of-degree-formula
 func BeamsLonLat(lon, lat float64, heading float32, along_track, across_track []float32, coef GeoCoefficients) LonLat {
     var (
         acr_trck float64
         aln_trck float64
         lonlat LonLat
+        deg2rad float64
     )
 
-    lat_rad := math.radians(lat)
-    head_rad := math.radians(heading)
+    deg2rad = math.Pi / 180.0
+
+    lat_rad := deg2rad * lat
+    head_rad := deg2rad * float64(heading)
 
     // latitude metres scale factor
     lat_sf := 
         coef.A -
-        coef.B * math.cos(2.0 * lat_rad) +
-        coef.C * math.cos(4.0 * lat_rad) -
-        coef.D * math.cos(6.0 * lat_rad)
+        coef.B * math.Cos(2.0 * lat_rad) +
+        coef.C * math.Cos(4.0 * lat_rad) -
+        coef.D * math.Cos(6.0 * lat_rad)
 
     // longitude metres scale factor
     lon_sf :=
-        coef.E * math.cos(lat_rad) - 
-        coef.F * math.cos(3.0 * lat_rad) +
-        coef.G * math.cos(5.0 * lat_rad)
+        coef.E * math.Cos(lat_rad) - 
+        coef.F * math.Cos(3.0 * lat_rad) +
+        coef.G * math.Cos(5.0 * lat_rad)
 
-    delta_x := math.sin(head_rad)
-    delta_y := math.cos(head_rad)
+    delta_x := math.Sin(head_rad)
+    delta_y := math.Cos(head_rad)
 
     n := len(along_track)
     lon2 := make([]float64, len(along_track))
@@ -74,8 +80,8 @@ func BeamsLonLat(lon, lat float64, heading float32, along_track, across_track []
     for i := 0; i < n; i++ {
         acr_trck = float64(across_track[i])
         aln_trck = float64(along_track[i])
-        lon2[i] = lon + delta_y / lon_sf * acr_track + delta_x / lon_sf * aln_trck
-        lat2[i] = lat - delta_x / lat_sf * acr_track + delta_y / lat_sf * aln_trck
+        lon2[i] = lon + delta_y / lon_sf * acr_trck + delta_x / lon_sf * aln_trck
+        lat2[i] = lat - delta_x / lat_sf * acr_trck + delta_y / lat_sf * aln_trck
     }
 
     lonlat.Longitude = lon2
