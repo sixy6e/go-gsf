09af71af65b7e95c7c9abfc257349883092a31a8
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2024-01-09 11:31:06 +1100

    Updated: EM4 struct now defines arrays and 2d arrays instead of scalars and 1d arrays

--- em4.go
+++ em4.go
@@ -13,59 +13,59 @@ import (
 // TODO; align into 64bit chunks
 // the spec says binary integers are stored as either 1-byte unsigned, 2-byte signed or unsigned, or 4-byte signed
 type EM4 struct {
-	ModelNumber                       int16
-	PingCounter                       int16
-	SerialNumber                      int16
-	SurfaceVelocity                   float32
-	TransducerDepth                   float64
-	ValidDetections                   int16
-	SamplingFrequency                 float64
-	DopplerCorrectionScale            int32
-	VehicleDepth                      float32
-	TransmitSectors                   int16
-	TiltAngle                         []float32
-	FocusRange                        []float32
-	SignalLength                      []float64
-	TransmitDelay                     []float64
-	CenterFrequency                   []float64
-	MeanAbsorption                    []float32
-	WaveformId                        []uint8
-	SectorNumber                      []uint8
-	SignalBandwith                    []float64
-	RunTimeModelNumber                int16
-	RunTimeDatagramTime               time.Time
-	RunTimePingCounter                int16
-	RunTimeSerialNumber               int16
-	RunTimeOperatorStationStatus      uint8
-	RunTimeProcessingUnitStatus       uint8
-	RunTimeBspStatus                  uint8
-	RunTimeHeadTransceiverStatus      uint8
-	RunTimeMode                       uint8
-	RunTimeFilterId                   uint8
-	RunTimeMinDepth                   float32
-	RunTimeMaxDepth                   float32
-	RunTimeAbsorption                 float32
-	RunTimeTransmitPulseLength        float32
-	RunTimeTransmitBeamWidth          float32
-	RunTimeTransmitPowerReduction     uint8
-	RunTimeReceiveBeamWidth           float32
-	RunTimeReceiveBandwidth           float32
-	RunTimeReceiveFixedGain           uint8
-	RunTimeTvgCrossOverAngle          uint8
-	RunTimeSsvSource                  uint8
-	RunTimeMaxPortSwathWidth          int16
-	RunTimeBeamSpacing                uint8
-	RunTimeMaxPortCoverage            uint8
-	RunTimeStabilization              uint8
-	RunTimeMaxStbdCoverage            uint8
-	RunTimeMaxStdbSwathWidth          int16
-	RunTimeTransmitAlongTilt          float32
-	RunTimeFilterId2                  uint8
-	ProcessorUnitCpuLoad              uint8
-	ProcessorUnitSensorStatus         uint16
-	ProcessorUnitAchievedPortCoverage uint8
-	ProcessorUnitAchievedStbdCoverage uint8
-	ProcessorUnitYawStabilization     float32
+	ModelNumber                       []int16
+	PingCounter                       []int16
+	SerialNumber                      []int16
+	SurfaceVelocity                   []float32
+	TransducerDepth                   []float64
+	ValidDetections                   []int16
+	SamplingFrequency                 []float64
+	DopplerCorrectionScale            []int32
+	VehicleDepth                      []float32
+	TransmitSectors                   []int16
+	TiltAngle                         [][]float32
+	FocusRange                        [][]float32
+	SignalLength                      [][]float64
+	TransmitDelay                     [][]float64
+	CenterFrequency                   [][]float64
+	MeanAbsorption                    [][]float32
+	WaveformId                        [][]uint8
+	SectorNumber                      [][]uint8
+	SignalBandwith                    [][]float64
+	RunTimeModelNumber                []int16
+	RunTimeDatagramTime               []time.Time
+	RunTimePingCounter                []int16
+	RunTimeSerialNumber               []int16
+	RunTimeOperatorStationStatus      []uint8
+	RunTimeProcessingUnitStatus       []uint8
+	RunTimeBspStatus                  []uint8
+	RunTimeHeadTransceiverStatus      []uint8
+	RunTimeMode                       []uint8
+	RunTimeFilterId                   []uint8
+	RunTimeMinDepth                   []float32
+	RunTimeMaxDepth                   []float32
+	RunTimeAbsorption                 []float32
+	RunTimeTransmitPulseLength        []float32
+	RunTimeTransmitBeamWidth          []float32
+	RunTimeTransmitPowerReduction     []uint8
+	RunTimeReceiveBeamWidth           []float32
+	RunTimeReceiveBandwidth           []float32
+	RunTimeReceiveFixedGain           []uint8
+	RunTimeTvgCrossOverAngle          []uint8
+	RunTimeSsvSource                  []uint8
+	RunTimeMaxPortSwathWidth          []int16
+	RunTimeBeamSpacing                []uint8
+	RunTimeMaxPortCoverage            []uint8
+	RunTimeStabilization              []uint8
+	RunTimeMaxStbdCoverage            []uint8
+	RunTimeMaxStdbSwathWidth          []int16
+	RunTimeTransmitAlongTilt          []float32
+	RunTimeFilterId2                  []uint8
+	ProcessorUnitCpuLoad              []uint8
+	ProcessorUnitSensorStatus         []uint16
+	ProcessorUnitAchievedPortCoverage []uint8
+	ProcessorUnitAchievedStbdCoverage []uint8
+	ProcessorUnitYawStabilization     []float32
 }
 
 func DecodeEM4Specific(reader *bytes.Reader) (sensor_data EM4) {
@@ -210,68 +210,68 @@ func DecodeEM4Specific(reader *bytes.Reader) (sensor_data EM4) {
 	_ = binary.Read(reader, binary.BigEndian, &proc_buffer)
 
 	// populate generic
-	sensor_data.ModelNumber = buffer.ModelNumber
-	sensor_data.PingCounter = buffer.PingCounter
-	sensor_data.SerialNumber = buffer.SerialNumber
-	sensor_data.SurfaceVelocity = float32(buffer.SurfaceVelocity) / float32(10)
-	sensor_data.TransducerDepth = float64(buffer.TransducerDepth) / float64(20000)
-	sensor_data.ValidDetections = buffer.ValidDetections
-	sensor_data.SamplingFrequency = float64(buffer.SamplingFrequency1) + float64(buffer.SamplingFrequency2)/float64(4_000_000_000)
-	sensor_data.DopplerCorrectionScale = buffer.DopplerCorrectionScale
-	sensor_data.VehicleDepth = float32(buffer.VehicleDepth) / float32(1000)
-	sensor_data.TransmitSectors = buffer.TransmitSectors
+	sensor_data.ModelNumber = []int16{buffer.ModelNumber}
+	sensor_data.PingCounter = []int16{buffer.PingCounter}
+	sensor_data.SerialNumber = []int16{buffer.SerialNumber}
+	sensor_data.SurfaceVelocity = []float32{float32(buffer.SurfaceVelocity) / float32(10)}
+	sensor_data.TransducerDepth = []float64{float64(buffer.TransducerDepth) / float64(20000)}
+	sensor_data.ValidDetections = []int16{buffer.ValidDetections}
+	sensor_data.SamplingFrequency = []float64{float64(buffer.SamplingFrequency1) + float64(buffer.SamplingFrequency2)/float64(4_000_000_000)}
+	sensor_data.DopplerCorrectionScale = []int32{buffer.DopplerCorrectionScale}
+	sensor_data.VehicleDepth = []float32{float32(buffer.VehicleDepth) / float32(1000)}
+	sensor_data.TransmitSectors = []int16{buffer.TransmitSectors}
 
 	// populate sector info
-	sensor_data.TiltAngle = sector_buffer.TiltAngle
-	sensor_data.FocusRange = sector_buffer.FocusRange
-	sensor_data.SignalLength = sector_buffer.SignalLength
-	sensor_data.TransmitDelay = sector_buffer.TransmitDelay
-	sensor_data.CenterFrequency = sector_buffer.CenterFrequency
-	sensor_data.MeanAbsorption = sector_buffer.MeanAbsorption
-	sensor_data.WaveformId = sector_buffer.WaveformId
-	sensor_data.SectorNumber = sector_buffer.SectorNumber
-	sensor_data.SignalBandwith = sector_buffer.SignalBandwith
+	sensor_data.TiltAngle = [][]float32{sector_buffer.TiltAngle}
+	sensor_data.FocusRange = [][]float32{sector_buffer.FocusRange}
+	sensor_data.SignalLength = [][]float64{sector_buffer.SignalLength}
+	sensor_data.TransmitDelay = [][]float64{sector_buffer.TransmitDelay}
+	sensor_data.CenterFrequency = [][]float64{sector_buffer.CenterFrequency}
+	sensor_data.MeanAbsorption = [][]float32{sector_buffer.MeanAbsorption}
+	sensor_data.WaveformId = [][]uint8{sector_buffer.WaveformId}
+	sensor_data.SectorNumber = [][]uint8{sector_buffer.SectorNumber}
+	sensor_data.SignalBandwith = [][]float64{sector_buffer.SignalBandwith}
 
 	// populate runtime info
-	sensor_data.RunTimeModelNumber = runtime_buffer.RunTimeModelNumber
-	sensor_data.RunTimeDatagramTime = time.Unix(
+	sensor_data.RunTimeModelNumber = []int16{runtime_buffer.RunTimeModelNumber}
+	sensor_data.RunTimeDatagramTime = []time.Time{time.Unix(
 		int64(runtime_buffer.RunTimeDatagramTime_sec),
 		int64(runtime_buffer.RunTimeDatagramTime_nsec),
-	)
-	sensor_data.RunTimePingCounter = runtime_buffer.RunTimePingCounter
-	sensor_data.RunTimeSerialNumber = runtime_buffer.RunTimeSerialNumber
-	sensor_data.RunTimeOperatorStationStatus = runtime_buffer.RunTimeOperatorStationStatus
-	sensor_data.RunTimeProcessingUnitStatus = runtime_buffer.RunTimeProcessingUnitStatus
-	sensor_data.RunTimeBspStatus = runtime_buffer.RunTimeBspStatus
-	sensor_data.RunTimeHeadTransceiverStatus = runtime_buffer.RunTimeHeadTransceiverStatus
-	sensor_data.RunTimeMode = runtime_buffer.RunTimeMode
-	sensor_data.RunTimeFilterId = runtime_buffer.RunTimeFilterId
-	sensor_data.RunTimeMinDepth = float32(runtime_buffer.RunTimeMinDepth)
-	sensor_data.RunTimeMaxDepth = float32(runtime_buffer.RunTimeMaxDepth)
-	sensor_data.RunTimeAbsorption = float32(runtime_buffer.RunTimeAbsorption) / float32(100)
-	sensor_data.RunTimeTransmitPulseLength = float32(runtime_buffer.RunTimeTransmitPulseLength)
-	sensor_data.RunTimeTransmitBeamWidth = float32(runtime_buffer.RunTimeTransmitBeamWidth) / float32(10)
-	sensor_data.RunTimeTransmitPowerReduction = runtime_buffer.RunTimeTransmitPowerReduction
-	sensor_data.RunTimeReceiveBeamWidth = float32(runtime_buffer.RunTimeReceiveBeamWidth) / float32(10)
-	sensor_data.RunTimeReceiveBandwidth = float32(runtime_buffer.RunTimeReceiveBandwidth) * float32(50)
-	sensor_data.RunTimeReceiveFixedGain = runtime_buffer.RunTimeReceiveFixedGain
-	sensor_data.RunTimeTvgCrossOverAngle = runtime_buffer.RunTimeTvgCrossOverAngle
-	sensor_data.RunTimeSsvSource = runtime_buffer.RunTimeSsvSource
-	sensor_data.RunTimeMaxPortSwathWidth = runtime_buffer.RunTimeMaxPortSwathWidth
-	sensor_data.RunTimeBeamSpacing = runtime_buffer.RunTimeBeamSpacing
-	sensor_data.RunTimeMaxPortCoverage = runtime_buffer.RunTimeMaxPortCoverage
-	sensor_data.RunTimeStabilization = runtime_buffer.RunTimeStabilization
-	sensor_data.RunTimeMaxStbdCoverage = runtime_buffer.RunTimeMaxStbdCoverage
-	sensor_data.RunTimeMaxStdbSwathWidth = runtime_buffer.RunTimeMaxStdbSwathWidth
-	sensor_data.RunTimeTransmitAlongTilt = float32(runtime_buffer.RunTimeTransmitAlongTilt) / float32(100)
-	sensor_data.RunTimeFilterId2 = runtime_buffer.RunTimeFilterId2
+	)}
+	sensor_data.RunTimePingCounter = []int16{runtime_buffer.RunTimePingCounter}
+	sensor_data.RunTimeSerialNumber = []int16{runtime_buffer.RunTimeSerialNumber}
+	sensor_data.RunTimeOperatorStationStatus = []uint8{runtime_buffer.RunTimeOperatorStationStatus}
+	sensor_data.RunTimeProcessingUnitStatus = []uint8{runtime_buffer.RunTimeProcessingUnitStatus}
+	sensor_data.RunTimeBspStatus = []uint8{runtime_buffer.RunTimeBspStatus}
+	sensor_data.RunTimeHeadTransceiverStatus = []uint8{runtime_buffer.RunTimeHeadTransceiverStatus}
+	sensor_data.RunTimeMode = []uint8{runtime_buffer.RunTimeMode}
+	sensor_data.RunTimeFilterId = []uint8{runtime_buffer.RunTimeFilterId}
+	sensor_data.RunTimeMinDepth = []float32{float32(runtime_buffer.RunTimeMinDepth)}
+	sensor_data.RunTimeMaxDepth = []float32{float32(runtime_buffer.RunTimeMaxDepth)}
+	sensor_data.RunTimeAbsorption = []float32{float32(runtime_buffer.RunTimeAbsorption) / float32(100)}
+	sensor_data.RunTimeTransmitPulseLength = []float32{float32(runtime_buffer.RunTimeTransmitPulseLength)}
+	sensor_data.RunTimeTransmitBeamWidth = []float32{float32(runtime_buffer.RunTimeTransmitBeamWidth) / float32(10)}
+	sensor_data.RunTimeTransmitPowerReduction = []uint8{runtime_buffer.RunTimeTransmitPowerReduction}
+	sensor_data.RunTimeReceiveBeamWidth = []float32{float32(runtime_buffer.RunTimeReceiveBeamWidth) / float32(10)}
+	sensor_data.RunTimeReceiveBandwidth = []float32{float32(runtime_buffer.RunTimeReceiveBandwidth) * float32(50)}
+	sensor_data.RunTimeReceiveFixedGain = []uint8{runtime_buffer.RunTimeReceiveFixedGain}
+	sensor_data.RunTimeTvgCrossOverAngle = []uint8{runtime_buffer.RunTimeTvgCrossOverAngle}
+	sensor_data.RunTimeSsvSource = []uint8{runtime_buffer.RunTimeSsvSource}
+	sensor_data.RunTimeMaxPortSwathWidth = []int16{runtime_buffer.RunTimeMaxPortSwathWidth}
+	sensor_data.RunTimeBeamSpacing = []uint8{runtime_buffer.RunTimeBeamSpacing}
+	sensor_data.RunTimeMaxPortCoverage = []uint8{runtime_buffer.RunTimeMaxPortCoverage}
+	sensor_data.RunTimeStabilization = []uint8{runtime_buffer.RunTimeStabilization}
+	sensor_data.RunTimeMaxStbdCoverage = []uint8{runtime_buffer.RunTimeMaxStbdCoverage}
+	sensor_data.RunTimeMaxStdbSwathWidth = []int16{runtime_buffer.RunTimeMaxStdbSwathWidth}
+	sensor_data.RunTimeTransmitAlongTilt = []float32{float32(runtime_buffer.RunTimeTransmitAlongTilt) / float32(100)}
+	sensor_data.RunTimeFilterId2 = []uint8{runtime_buffer.RunTimeFilterId2}
 
 	// populate processor unit info
-	sensor_data.ProcessorUnitCpuLoad = proc_buffer.ProcessorUnitCpuLoad
-	sensor_data.ProcessorUnitSensorStatus = proc_buffer.ProcessorUnitSensorStatus
-	sensor_data.ProcessorUnitAchievedPortCoverage = proc_buffer.ProcessorUnitAchievedPortCoverage
-	sensor_data.ProcessorUnitAchievedStbdCoverage = proc_buffer.ProcessorUnitAchievedStbdCoverage
-	sensor_data.ProcessorUnitYawStabilization = float32(proc_buffer.ProcessorUnitYawStabilization) / float32(100)
+	sensor_data.ProcessorUnitCpuLoad = []uint8{proc_buffer.ProcessorUnitCpuLoad}
+	sensor_data.ProcessorUnitSensorStatus = []uint16{proc_buffer.ProcessorUnitSensorStatus}
+	sensor_data.ProcessorUnitAchievedPortCoverage = []uint8{proc_buffer.ProcessorUnitAchievedPortCoverage}
+	sensor_data.ProcessorUnitAchievedStbdCoverage = []uint8{proc_buffer.ProcessorUnitAchievedStbdCoverage}
+	sensor_data.ProcessorUnitYawStabilization = []float32{float32(proc_buffer.ProcessorUnitYawStabilization) / float32(100)}
 
 	return sensor_data
 }
