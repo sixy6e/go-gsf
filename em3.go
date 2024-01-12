be5d44502945ecb4cce6fc6c6c1e0e007dfab8ab
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2024-01-12 15:31:53 +1100

    Initial commit of em3.go

---
+++ em3.go
@@ -0,0 +1,18 @@
+package gsf
+
+// TODO; confirm the fields with the spec
+// fields were simply copied from EM4Imagery
+type EM3Imagery struct {
+	SamplingFrequency   []float64
+	MeanAbsorption      []float32
+	TransmitPulseLength []float32
+	RangeNorm           []uint16
+	StartTvgRamp        []uint16
+	StopTvgRamp         []uint16
+	BackscatterN        []float32
+	BackscatterO        []float32
+	TransmitBeamWidth   []float32
+	TvgCrossOver        []float32
+	offset              []int16
+	scale               []int16
+}
