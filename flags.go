335c26dd315c4bc3c5f80028b7601e7eeb9bfc0c
Josh Sixsmith <josh.sixsmith+GitHub@gmail.com>
2024-01-16 11:12:00 +1100

    Removed returning the byte count

--- flags.go
+++ flags.go
@@ -9,17 +9,17 @@ import (
 // The length of the returned slice is determined by the input
 // number of beams.
 // Each element indicates whether or not the beam contains usable data.
-func DecodeBeamFlagsArray(reader *bytes.Reader, nbeams uint16) ([]uint8, int64) {
+func DecodeBeamFlagsArray(reader *bytes.Reader, nbeams uint16) []uint8 {
 	var (
-		data    []uint8
-		n_bytes int64
+		data []uint8
+		// n_bytes int64
 	)
 
 	data = make([]uint8, nbeams)
-	n_bytes = 0
+	// n_bytes = 0
 
 	_ = binary.Read(reader, binary.BigEndian, &data)
-	n_bytes += 1 * int64(nbeams)
+	// n_bytes += 1 * int64(nbeams)
 
-	return data, n_bytes
+	return data //, n_bytes
 }
